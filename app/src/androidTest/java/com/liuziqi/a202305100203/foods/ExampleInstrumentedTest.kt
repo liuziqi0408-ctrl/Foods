d668b9f30dd90a91e9a7582d0359001488605a42
liuziqi <1246496583@qq.com>
2026-01-02 11:59:12 +0800

    1

---
+++ app/src/androidTest/java/com/liuziqi/a202305100203/foods/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.liuziqi.a202305100203.foods
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.liuziqi.a202305100203.foods", appContext.packageName)
+    }
+}
